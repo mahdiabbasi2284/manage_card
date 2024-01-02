fbad7575a499d94b1e884169371372c98acb1390
mahdiabbasi2284 <mahdi.abbasi2284@gmail.com>
2024-01-02 12:10:55 -0800

    floating action

--- lib/for_test.dart
+++ lib/for_test.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 class ForTest extends StatefulWidget {
@@ -15,92 +17,150 @@ TextEditingController cardNumberController4 = TextEditingController();
 class _ForTestState extends State<ForTest> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                width: double.infinity,
-                height: 70,
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: cardNumberController1,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: '',
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    if (value.length == 4) {
-                      FocusScope.of(context).nextFocus();
-                    }
-                  },
-                ),
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: Align(
+          alignment: Alignment.bottomRight,
+          child: FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {
+                  showDialogWithPage(context);
+                },
+                child: Icon(Icons.add,color: Colors.white,),
               ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: cardNumberController2,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: '',
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    if (value.length == 4) {
-                      FocusScope.of(context).nextFocus();
-                    }
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: cardNumberController3,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: '',
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    if (value.length == 4) {
-                      FocusScope.of(context).nextFocus();
-                    }
-                  },
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: cardNumberController4,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: '',
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    if (value.length == 4) {
-                      FocusScope.of(context).nextFocus();
-                    }
-                  },
-                ),
-              ),
-            ),
-          ],
         ),
+        body: Container()
       ),
     );
   }
+}
+
+void showDialogWithPage(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('اضافه کردن کارت'),
+        content: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: 500,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(8),
+                          child: TextFormField(
+                            textAlign: TextAlign.center,
+                            controller: cardNumberController1,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(2, 1, 2, 1),
+                              labelText: '',
+                              border: OutlineInputBorder(),
+                            ),
+                            onChanged: (value) {
+                              if (value.length == 4) {
+                                FocusScope.of(context).nextFocus();
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          child: Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.all(8),
+                            child: TextFormField(
+                              textAlign: TextAlign.center,
+                              controller: cardNumberController2,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.fromLTRB(2, 1, 2, 1),
+                                labelText: '',
+                                border: OutlineInputBorder(),
+                              ),
+                              onChanged: (value) {
+                                if (value.length == 4) {
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.all(8),
+                            child: TextFormField(
+                              textAlign: TextAlign.center,
+                              controller: cardNumberController3,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.fromLTRB(2, 1, 2, 1),
+                                labelText: '',
+                                border: OutlineInputBorder(),
+                              ),
+                              onChanged: (value) {
+                                if (value.length == 4) {
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.all(8),
+                            child: TextFormField(
+                              textAlign: TextAlign.center,
+                              controller: cardNumberController4,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.fromLTRB(2, 1, 2, 1),
+                                labelText: '',
+                                border: OutlineInputBorder(),
+                              ),
+                              onChanged: (value) {
+                                if (value.length == 4) {
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                  Expanded(
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(8),
+                          child: TextFormField(
+                            textAlign: TextAlign.center,
+                            controller: cardNumberController2,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(2, 1, 2, 1),
+                              border: OutlineInputBorder(),
+                            ),
+                            onChanged: (value) {
+                              if (value.length == 4) {
+                                FocusScope.of(context).nextFocus();
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                ],
+              ),
+            );
+          },
+        ),
+      );
+    },
+  );
 }
